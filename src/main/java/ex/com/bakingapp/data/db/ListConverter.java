3e6f4d92299444d8f699b79fc3404cc74a7d195a
Nick <fabdadv@gmai.com>
2018-05-17 09:35:55 +0700

    Fork from AAC BasicSample

---
+++ src/main/java/ex/com/bakingapp/data/db/ListConverter.java
@@ -0,0 +1,19 @@
+package ex.com.bakingapp.data.db;
+
+import android.arch.persistence.room.TypeConverter;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListConverter {
+    @TypeConverter
+    public String fromList(List<String> stringList) {
+        String data = null;
+        for (String str : stringList) { data.concat(str + "\n"); }
+//                stringList.stream().collect(Collectors.joining("\n"));
+        return data;
+    }
+    @TypeConverter
+    public List<String> toList(String data) {
+        return Arrays.asList(data.split("\n"));
+    }
+}
