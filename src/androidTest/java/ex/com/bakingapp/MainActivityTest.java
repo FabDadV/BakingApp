4e1a8a0105849b3d9494c47a3c1ae52acfd9fe38
Nick <fabdadv@gmai.com>
2018-08-29 19:33:55 +0700

    add Espresso tests

--- src/androidTest/java/ex/com/bakingapp/MainActivityTest.java
+++ src/androidTest/java/ex/com/bakingapp/MainActivityTest.java
@@ -1,6 +1,8 @@
 package ex.com.bakingapp;
 
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -29,6 +31,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
@@ -55,7 +58,6 @@ public class MainActivityTest {
                                 0),
                         isDisplayed()));
         textView.check(matches(withText("Yellow Cake")));
-
     }
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
@@ -74,5 +76,30 @@ public class MainActivityTest {
                         && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
+    }
+    @Test
+    public void bakingAppTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.rv_list), withContentDescription("RecycleView"),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                1)));
+        recyclerView.perform(actionOnItemAtPosition(1, click()));
+
+        ViewInteraction textView = onView(
+                allOf(withText("8"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("8")));
+    }
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("ex.com.bakingapp", appContext.getPackageName());
     }
 }
