d4111a7db4ac4dd92cf5b9d9c54d3b2a1a7b03c3
Nick <fabdadv@gmai.com>
2018-08-29 07:52:23 +0700

    add Master/Detail flow (RecipeActivity)

--- src/main/java/ex/com/bakingapp/ui/RecipeActivity.java
+++ src/main/java/ex/com/bakingapp/ui/RecipeActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class ListActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
