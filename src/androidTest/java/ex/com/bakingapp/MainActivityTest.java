31a0581954c27e88724106510c8bd7d3eb31eeb2
Nick <fabdadv@gmai.com>
2018-08-29 07:57:03 +0700

    add Master/Detail flow (RecipeActivity)

--- src/androidTest/java/ex/com/bakingapp/MainActivityTest.java
+++ src/androidTest/java/ex/com/bakingapp/MainActivityTest.java
@@ -1,6 +1,8 @@
 package ex.com.bakingapp;
 
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -29,6 +31,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertEquals;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
 
@@ -75,4 +78,13 @@ public class MainActivityTest {
             }
         };
     }
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("ex.com.bakingapp", appContext.getPackageName());
+    }
+
 }
